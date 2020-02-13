992213671b3c7230c56b60c7b0ef0cff1afdf8a9
Victor Jaimes-Puente <victor.jaimes.puente@gmail.com>
2020-02-13 16:01:20 -0600

    finished iterating arrays

--- js/iterating.js
+++ js/iterating.js
@@ -1,4 +1,4 @@
-(function(){
+
     "use strict";
 
     /**
@@ -6,30 +6,36 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+var names1 = ["Ay", "Bee", "Cii", "Dee", "Eeei",];
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-
+    console.log(names1.length);
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-
+for (var i=0; i < names1.length; i++){
+        console.log(names1[i]);
+    }
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
-
+    for (var i=0; i < names1.length; i++) {
+        console.log(names1[i]);
+    }
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
-
+        for (var i=0; i < names1.length; i++) {
+            console.log(names1[i]);
+        }
     /**
      * TODO:
      * Create the following three functions, each will accept an array and
@@ -43,5 +49,15 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
-
-})();
+    function index0(array) {
+        return array[0];
+    }
+    function index1(array) {
+        return array[1];
+    }
+    function index2(array) {
+        return array[2];
+    }
+    console.log(index0(names1));
+    console.log(index1(names1));
+    console.log(index2(names1));
