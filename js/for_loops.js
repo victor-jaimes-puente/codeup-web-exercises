990b7186388425e3e4e9929fd3c21eb1f41d45b2
Victor Jaimes-Puente <victor.jaimes.puente@gmail.com>
2020-02-13 11:53:25 -0600

    fin forloops

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,24 +5,103 @@ will create a function that prints multiplication table of a number based on the
 
  */
 
-function showMultiplicationTable(input){
-    for(var i = 1; i <= 10; i++){
-        var equals = input * i;
+// function showMultiplicationTable(input){
+//     for(var i = 1; i <= 10; i++){
+//         var equals = input * i;
+//
+//         var all1 = input + " X " + i + " = " + equals;
+//
+//         console.log(all1);
+//     }
+// }
+//
+// console.log(showMultiplicationTable(7));
+// function randomNumber(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+// }
+//
+// for(var i = 0; i < 10; i++){
+//
+//
+//     var randomNumber = Math.floor(Math.random() * 180) + 20;
+//
+//     if(randomNumber % 2 === 0){
+//         console.log(randomNumber + ' is even');
+//     } else {
+//         console.log(randomNumber + ' is odd');
+//     }
+//
+// }
+//
+// for( var v = 0; v < 10; v++ ){
+//     var randomNumber = Math.floor(Math.random() * 180) +20;
+//     if(randomNumber % 2 === 0){
+//         console.log(randomNumber + ' is even');
+//     }else{
+//         console.log(randomNumber + " is odd");
+//     }
+// }
 
-        var all1 = input + " X " + i + " = " + equals;
+//
+// for ( var i = 0; i < 10; i++){
+//     console.log(i.toString().repeat(i));
+// }
 
-        console.log(all1);
-    }
+for ( var i = 100; i > 0; i-=5){
+    console.log(i);
 }
 
-console.log(showMultiplicationTable(7));
-function randomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
 
-for (var i = 1; i <= 10; i++){
-    var numList = (randomNumber(20,200) + " =");
-    console.log(numList);
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
