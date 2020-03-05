02593e99fb14d2d013ee6bb1927e34b03a5a48bf
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-04 19:35:30 -0600

    started weather map

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,12 +1,14 @@
 'use strict';
 console.log('hello');
 $.ajax('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+ darkSkyKey +'/37.8267,-122.4233').done(function (data) {
-    createCard(data);
+    // createCard(data);
+    console.log(data.currently.summary);
 });
 
-function createCard(data) {
-var result = "";
-    data.forEach(function (set) {
-        result += set.currently;
-    })
-}
+// function createCard(data) {
+// var result = "";
+//     data.forEach(function (set) {
+//         result +='<div class="card-body"></div> ';
+//         result += set.currently;
+//     })
+// }
