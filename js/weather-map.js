d1799c0835b39451b797fbe38c48d5e292410e7a
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-08 17:50:30 -0500

    crated variables for 3 days and created function for time correction

--- js/weather-map.js
+++ js/weather-map.js
@@ -1,10 +1,25 @@
 'use strict';
-console.log('hello');
-$.ajax('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/'+ darkSkyKey +'/37.8267,-122.4233').done(function (data) {
-    // createCard(data);
-    // console.log(data.currently.time * 1000);
-var dateObject = new Date(data.currently.time * 1000);
- var dateStringed = (dateObject.toString());
- return dateStringed
+$.ajax('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' + darkSkyKey + '/37.8267,-122.4233').done(function (data) {
+// dateCorrectionFunction
+    function dateCorrection(time) {
+        var dateObject = new Date(time * 1000);
+        var dateStringed = (dateObject.toString());
+        console.log(dateStringed);
+        return dateStringed;
+    }
+
+// three days including today
+    var weatherToday = data.daily.data[0];
+    var weatherTom = data.daily.data[1];
+    var weatherday3 = data.daily.data[2];
+// testing date crrection function
+
+// dateCorrection(weatherToday.time);
+
+// dateVariable Creation
+    var todayDate = dateCorrection(weatherToday.time);
+    var day2Date = dateCorrection(weatherTom.time);
+    var day3Date = dateCorrection(weatherday3.time);
+
 });
-console.log(dateStringed);
+
