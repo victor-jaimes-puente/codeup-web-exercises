1e40d331f00d2d5d1df35830540a031d9aa7ba61
VictorJP <victor.jaimes.puente@gmail.com>
2020-03-08 19:42:43 -0500

    added first cards as test, can remove without issues. Finished all variables for 3 days which include necessary properties in the form of an array not an object.

--- js/weather-map.js
+++ js/weather-map.js
@@ -14,12 +14,51 @@ $.ajax('https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/' +
     var weatherday3 = data.daily.data[2];
 // testing date crrection function
 
-// dateCorrection(weatherToday.time);
+    // dateCorrection(weatherToday.time);
 
 // dateVariable Creation
     var todayDate = dateCorrection(weatherToday.time);
     var day2Date = dateCorrection(weatherTom.time);
     var day3Date = dateCorrection(weatherday3.time);
 
+// var for today summary string
+    var summaryToday = weatherToday.summary;
+    var summaryDay2 = weatherTom.summary;
+    var summaryday3 = weatherday3.summary;
+    console.log(summaryDay2);
+    console.log(summaryToday);
+    console.log(summaryday3);
+// var set for icons
+    var todayIcon = weatherToday.icon;
+    var tomorrowIcon = weatherTom.icon;
+    var day3Icon = weatherday3.icon;
+    console.log(todayIcon);
+    console.log(tomorrowIcon);
+    console.log(day3Icon);
+// var set for wind
+    var todayWind = weatherToday.windSpeed;
+    var tomorrowWind = weatherTom.windSpeed;
+    var day3Wind = weatherday3.windSpeed;
+    console.log(todayWind);
+    console.log(tomorrowWind);
+    console.log(day3Wind);
+// var set for pressure
+    var todayPressure = weatherToday.pressure;
+    var tomorrowPressure = weatherTom.pressure;
+    var day3Pressure = weatherday3.pressure;
+    console.log(todayPressure);
+    console.log(tomorrowPressure);
+    console.log(day3Pressure);
+    console.log(data.daily.data[0]);
+// var set array of all
+    var todayAll = [todayDate, todayPressure, todayWind, todayIcon, summaryToday];
+    var tomorrowAll = [day2Date, tomorrowPressure, tomorrowWind, tomorrowIcon, summaryday3];
+    var day3All = [day3Date, day3Pressure, day3Wind, day3Icon, summaryday3];
+    console.log(todayAll);
+    console.log(tomorrowAll);
+    console.log(day3All);
+    // console.log(data.daily.data[0]);
 });
+
+
 
