62291afa10e03f90560d255090e136e661c0703c
VictorJP <victor.jaimes.puente@gmail.com>
2020-05-25 22:50:06 -0500

    jsForLoop practice

--- js/for_loops.js
+++ js/for_loops.js
@@ -4,18 +4,20 @@
 will create a function that prints multiplication table of a number based on the input which should be within 1 - 10.
 
  */
+function hello() {
+    console.log("Hello")
+}
+function showMultiplicationTable(input){
+    for(var i = 1; i <= 10; i++){
+        var equals = input * i;
 
-// function showMultiplicationTable(input){
-//     for(var i = 1; i <= 10; i++){
-//         var equals = input * i;
-//
-//         var all1 = input + " X " + i + " = " + equals;
-//
-//         console.log(all1);
-//     }
-// }
-//
-// console.log(showMultiplicationTable(7));
+        var all1 = input + " X " + i + " = " + equals;
+
+        console.log(all1);
+    }
+}
+
+console.log(showMultiplicationTable(7));
 // function randomNumber(min, max) {
 //     min = Math.ceil(min);
 //     max = Math.floor(max);
